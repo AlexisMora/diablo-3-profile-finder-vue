dcf3ded783f037fd21b52b2b3a0e1be856a89ff4
Alexis Mora <alexis.mora.sanchez@gmail.com>
2021-01-26 19:04:19 +0100

    project setup but now i linted the code

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
+  methods: {
+    init () {
+      console.log('Hola uwu')
+    }
+  },
+  created () {
+    this.init()
+  },
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store/modules/oauth.js
@@ -0,0 +1,10 @@
+// @ is an alias for /src
+/* import * as oauth from '@/api/oauth'
+
+export default {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {}
+}
+*/
